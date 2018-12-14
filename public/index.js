176a47c968599b0cf0b03f5c9b0ba514aee3a4ce
r-a-c-h-e-l <rahazes@gmail.com>
2018-12-14 03:44:58 -0500

    cleanup

--- public/index.js
+++ public/index.js
@@ -1,34 +1,105 @@
+import { nextSnakeState, initialSnakeState as snake } from './Snek.js';
+import { calcX, calcY, initialBoardState as board } from './Board.js';
 
-// create canvas
-// build a SSnAkE
-// draw a snake
-// slither a snake
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+const initialSnake = snake();
+const boardArgs = {
+  width: windowWidth,
+  height: windowHeight,
+  cellsize: initialSnake.cellsize,
+}
+const initialBoard = board(boardArgs)
 
-// constants
-// const EAST = { x: 1, y: 0 };
-// const WEST = { x: -1, y: 0 };
-// const NORTH = { x: 0, y: -1 };
-// const SOUTH = { x: 0, y: 1 };
-// // initial state
-// {
-//   snake: [],
-//   food: { x: 16, y: 5 },
-//   direction: EAST,
-//   moves: [],
+// drawing context
+let ctx;
+
+// initial state for all components
+let state = {
+  snake: initialSnake,
+  board: initialBoard,
+  food: {},
+  directions: ['EAST'],
+}
+
+const initializeCanvas = ({ width, height}) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  document.body.appendChild(canvas);
+  ctx = canvas.getContext('2d');
+}
+
+
+function draw() {
+  // clear board
+  // draw food
+  // draw snake
+  // clear
+  ctx.fillStyle = '#232323'
+  ctx.fillRect(0, 0, state.board.width, state.board.height)
+
+  // draw snake
+  ctx.fillStyle = 'rgb(0,200,50)'
+  state.snake.body.forEach(p => {
+    ctx.fillRect(p.x, p.y, state.snake.cellsize, state.snake.cellsize)
+  })
+}
+
+function nextDraw() {
+  const nextSnake = nextSnakeState(state)
+  state = {
+    ...state,
+    snake: nextSnake.snake,
+    directions: nextSnake.directions,
+  }
+  console.log({ nextState: state });
+  draw();
+}
+
+// const tick = t1 => t2 => {
+//   if (t2 - t1 > 100) {
+//     nextDraw()
+//     window.requestAnimationFrame(step(t2))
+//   } else {
+//     window.requestAnimationFrame(step(t1))
+//   }
 // }
 
-// call new Game
-// on Controls.up fire game.controlY
-// on Controls.down fire game.controlY
-// on Controls.right fire game.controlX
-// on Controls.left fire game.controlX
-
-// import Board from './Board.js';
-//
-// const windowWidth = window.innerWidth;
-// const windowHeight = window.innerHeight;
-// console.log({ windowHeight, windowWidth});
-//
-// const board = new Board(windowWidth, windowHeight);
-// board.initializeBoard();
-// board.draw()
+window.addEventListener('keydown', (e) => {
+  switch(e.key) {
+    case 'ArrowUp':
+      console.log('north');
+      state = {
+        ...state,
+        directions: state.directions.concat(['NORTH'])
+      }
+      console.log({stateAfterKey: state});
+      break;
+    case 'ArrowDown':
+      console.log('south');
+      state = {
+        ...state,
+        directions: state.directions.concat(['SOUTH'])
+      }
+      break;
+    case 'ArrowRight':
+      state = {
+        ...state,
+        directions: state.directions.concat(['EAST'])
+      }
+      break;
+    case 'ArrowLeft':
+      state = {
+        ...state,
+        directions: state.directions.concat(['WEST'])
+      }
+      break;
+  }
+});
+initializeCanvas({width: windowWidth, height: windowHeight})
+draw();
+// window.requestAnimationFrame(tick(0))
+let intervalId;
+intervalId = window.setInterval(nextDraw, 100);
+window.stopDraw = () => window.clearInterval(intervalId)
